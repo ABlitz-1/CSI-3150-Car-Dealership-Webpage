6341f4d3ec511972026f67250944f2224729fb83
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-03-16 17:06:27 -0400

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-import { usedCars } from './usedCars-TH-1.js';
+const usedCars = require('./usedCars-TH-1.js');
 
 const target = document.getElementsByClassName('row');
-const htmlString = dataArray.map(usedCars => 
+const htmlString = dataArray.map(usedCar => 
   `<div class="pro-card">
-    <h1>${usedCars.year} ${usedCars.make} ${usedCars.model}</h1>
-    <h2>${usedCars.mileage} ${usedCars.color}</h2>
-    <p class="price">${usedCars.price}</p>
-    <p>Gas Mileage: ${usedCars.gasMileage}</p>
+    <h1>${usedCar.year} ${usedCar.make} ${usedCar.model}</h1>
+    <h2>${usedCar.mileage} ${usedCar.color}</h2>
+    <p class="price">${usedCar.price}</p>
+    <p>Gas Mileage: ${usedCar.gasMileage}</p>
     <p><button>Check Availability</button></p>
   </div>`
 ).join('');
