1a748a1fdcf360134f24b8f02c3a8f44061b5c0e
ABlitz-1 <145579941+ABlitz-1@users.noreply.github.com>
2026-03-16 16:18:14 -0400

    Create main.js, update index.html and main.css

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let usedCars = require('usedCars-TH-1.js');
+
+const target = document.getElementsByClassName('row');
+const htmlString = dataArray.map(usedCar => 
+  <div class="pro-card">
+    <h1>${usedCar.year} ${usedCar.make} ${usedCar.model}</h1>
+    <h2>${usedCar.mileage} ${usedCar.color}</h2>
+    <p class="price">${usedCar.price}</p>
+    <p>Gas Mileage: ${usedCar.gasMileage}</p>
+    <p><button>Check Availability</button></p>
+  </div>
+).join('');
+
+targetElement.innerHTML = htmlString;
